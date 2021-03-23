db933882ad46dffa7e6438b1de041736507493a5
VITALIY KAVUN <kavun.v@dbbest.com>
2021-03-23 12:43:05 +0200

    added repository level

---
+++ src/main/java/com/example/demo/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
